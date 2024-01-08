e4e42c3ac356aa4e810ef4c98fee45a86755b82b
Harini Seetharaman <harini.seetharaman@nice.com>
2024-01-08 18:05:01 +0530

    facade

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DepartmentService } from './design-patterns/facade/department.service';
+import { EmployeeService } from './design-patterns/facade/employee.service';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +12,14 @@ import { RouterOutlet } from '@angular/router';
   styles: ``,
 })
 export class AppComponent {
+/*  constructor (private empSvc : EmployeeService, 
+    private deptSvc: DepartmentService ) {
+      this.empSvc.getEmployees().subscribe((empl) => {
+        console.log(empl);
+      });
+
+      this.deptSvc.getDepartments().subscribe((dept) => {
+        console.log(dept);
+      });
+  }*/
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { ParentComponent } from './deferred-loading/parent.component';
 import { SingletonTestComponent } from './design-patterns/singleton-test/singleton-test.component';
+import { FacadeTestComponent } from './design-patterns/facade-test/facade-test.component';
 
 export const routes: Routes = [
   {
@@ -10,5 +11,9 @@ export const routes: Routes = [
   {
     path: 'singleton',
     loadComponent: () => SingletonTestComponent,
+  },
+  {
+    path: 'facade',
+    loadComponent: () => FacadeTestComponent,
   }
 ];
