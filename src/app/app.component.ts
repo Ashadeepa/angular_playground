9a968f813035e1d1a43ff7a25f02f28c37647386
Ashadeepa Debnath <ashadeepa.debnath@nice.com>
2024-02-25 10:46:57 +0530

    Tech Fest

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,23 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
 import { DepartmentService } from './design-patterns/facade/department.service';
 import { EmployeeService } from './design-patterns/facade/employee.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  template: `<router-outlet></router-outlet>`,
-  styles: ``,
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
+  template: `
+    <div class="angular-playground-text"> Angular Playground</div>
+    <nav>
+      <ul>
+        <li><a routerLink="/" routerLinkActive="active" ariaCurrentWhenActive="page">Home</a></li>
+        <li><a routerLink="/deferred-loading" routerLinkActive="active" ariaCurrentWhenActive="page">Deferred loading</a></li>
+      </ul>
+    </nav>
+    <router-outlet></router-outlet>`,
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
 
