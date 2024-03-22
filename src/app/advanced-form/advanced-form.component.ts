4e7e9f4978f23c254aabfac17c5f4da4a564df8b
Snehal Pise <Snehal.Kuchekar2@nice.com>
2024-03-22 15:20:57 +0530

    Initial commit

---
+++ src/app/advanced-form/advanced-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-advanced-form',
+  templateUrl: './advanced-form.component.html'
+})
+export class AdvancedFormComponent {
+  public submitForm(){
+    // logic to submit the form
+  }
+}
---
+++ src/app/dropdown/dropdown.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dropdown',
+  templateUrl: './dropdown.component.html'
+})
+export class DropdownComponent {
+  
+}
