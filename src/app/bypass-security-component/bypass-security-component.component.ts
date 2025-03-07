6d4b413740ac56082a484b49cb299cbf162de0a3
Shruti Karmarkar <Shruti.Karmarkar@nice.com>
2025-03-07 15:15:12 +0530

    problem statement

--- src/app/bypass-security-component/bypass-security-component.component.ts
+++ src/app/bypass-security-component/bypass-security-component.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { DomSanitizer, SafeResourceUrl, SafeUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-bypass-security-component',
@@ -10,26 +9,11 @@ import { DomSanitizer, SafeResourceUrl, SafeUrl } from '@angular/platform-browse
 })
 export class BypassSecurityComponentComponent {
   public dangerousUrl: string;
-  public trustedUrl: SafeUrl;
-  public dangerousVideoUrl: string | undefined;
-  public videoUrl: SafeResourceUrl | undefined;
 
-  constructor(private sanitizer: DomSanitizer) {
+  constructor() {
     // javascript: URLs are dangerous if attacker controlled.
     // Angular sanitizes them in data binding, but you can
     // explicitly tell Angular to trust this value:
     this.dangerousUrl = 'javascript:alert("Hi there")';
-    this.trustedUrl = sanitizer.bypassSecurityTrustUrl(this.dangerousUrl);
-    // this.updateVideoUrl('PUBnlbjZFAI');
-  }
-
-  updateVideoUrl(id: string) {
-    // Appending an ID to a YouTube URL is safe.
-    // Always make sure to construct SafeValue objects as
-    // close as possible to the input data so
-    // that it's easier to check if the value is safe.
-    this.dangerousVideoUrl = 'https://www.youtube.com/embed/' + id;
-    this.videoUrl =
-        this.sanitizer.bypassSecurityTrustResourceUrl(this.dangerousVideoUrl);
   }
 }
