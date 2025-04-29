c286541f099ed8421d4a73bad5ebf6b0e3e7600a
RuchikaGupta03 <Ruchika.Gupta@nice.com>
2025-04-29 21:41:13 +0530

    pwa-workshop

--- src/app/app-pwa/app-pwa.component.ts
+++ src/app/app-pwa/app-pwa.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-pwa',
   standalone: true,
-  imports: [],
+  imports: [CommonModule], // Import CommonModule to use *ngIf
   templateUrl: './app-pwa.component.html',
-  styleUrl: './app-pwa.component.scss'
+  styleUrls: ['./app-pwa.component.scss']
 })
 export class AppPwaComponent {
+  isImageVisible = false;
 
+  ngAfterViewInit() {
+    const images = document.querySelectorAll('.progressive-image');
+    images.forEach(img => {
+      const highResImage = new Image();
+      const dataSrc = img.getAttribute('data-src');
+      if (dataSrc) {
+        highResImage.src = dataSrc;
+        highResImage.onload = () => {
+          img.setAttribute('src', highResImage.src);
+        };
+      }
+    });
+  }
+
+  toggleImageVisibility() {
+    this.isImageVisible = !this.isImageVisible;
+  }
 }
