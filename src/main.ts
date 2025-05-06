e4409d00e099d9d77be8c7d06e3c996fff105d1f
RuchikaGupta03 <Ruchika.Gupta@nice.com>
2025-05-06 18:47:11 +0530

    pwa-workshop

--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,13 @@ import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+
+
+if ('serviceWorker' in navigator) {
+   navigator.serviceWorker.register('./ngsw-worker.js')
+    .then(function(registration) {
+      console.log('Service Worker registered with scope:', registration.scope);
+    }).catch(function(error) {
+    console.log('Service Worker registration failed:', error);
+  });
+}
